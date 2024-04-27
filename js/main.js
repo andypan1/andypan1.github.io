7b714e01b7a7d8107774c3725f23852baffb164e
Andy Pan <andypan@umich.edu>
2024-04-26 22:50:03 -0400

    added education

--- js/main.js
+++ js/main.js
@@ -7,3 +7,10 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
 });
+//education
+const acc = document.getElementsByClassName('contentbx');
+for(i = 0; i < acc.length; i++){
+    acc[i].addEventListener('click', function(){
+        this.classList.toggle("active")
+    });
+};
